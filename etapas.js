46c20bc0c1182bf698df2ca3798052ecadb4f90a
João Marcos Chaves <102380570+Joaomarcos7@users.noreply.github.com>
2022-10-29 02:40:56 -0300

    criação do objeto dos candidatos

---
+++ etapas.js
@@ -0,0 +1,30 @@
+const etapas=[
+    
+    {
+    titulo:'Presidente',
+    numeros:2,
+    candidatos:[
+        {
+            nome:'LULA',
+            partido:'PT',
+            fotos:[{
+                url:'imgs/candidato_13.jpeg',
+                legenda:'Presidente'
+            }]
+        },
+        {
+            nome:'BOLSONARO',
+            partido:'PL',
+            fotos:[{
+                url:'imgs/125728410_bosoreuters-2.jpg',
+                legenda:'Presidente'
+            }]
+        }
+    ]
+}
+
+]
+
+   
+
+
